061515318901489a3e29fd288c88b8e733804e0b
meow191204 <141601012+meow191204@users.noreply.github.com>
2023-11-16 03:23:38 +0900

    Delete C언어 실습 directory
    필요없어

--- C언어 실습/11.16/test-1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-extern int x,y;
-void cat()
-{
-
-    printf("두 수를 입력하시오 :");
-    scanf("%d %d",&x,&y);
-}
--- C언어 실습/9.14/trapezoid.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int a,b,h;
-    printf("밑변, 윗변, 높이를 입력하세요: ");
-    scanf("%d %d %d", &a,&b,&h);
-    printf("사다리꼴의 넓이는 : %f 입니다.\n" , ((a+b)*h)/2.0);
-    return 0 ;
-}
